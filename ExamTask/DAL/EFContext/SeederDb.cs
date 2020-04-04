abcddc35e2b6f978d04fc8c5f8f6c2b69eac9c58
Denis Yaremchuk <deniskoyaremchuk228@outlook.com>
2020-04-04 14:43:38 +0300

    added Identity, all classes, account controller and installed packages

---
+++ ExamTask/DAL/EFContext/SeederDb.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExamTask.DAL.Models;
+
+namespace ExamTask.DAL.EFContext
+{
+    public class SeederDb
+    {
+
+        public static void SeedRoles(RoleManager<DbRole> _roleManager)
+        {
+            var count = _roleManager.Roles.Count();
+
+            if (count <= 0)
+            {
+                var roleName = "User";
+                var result = _roleManager.CreateAsync(new DbRole
+                {
+                    Name = roleName
+                }).Result;
+
+                roleName = "Admin";
+                result = _roleManager.CreateAsync(new DbRole
+                {
+                    Name = roleName
+                }).Result;
+            }
+        }
+
+        public static void SeedSportEquipment(EFDbContext _context)
+        {
+            var count = _context.Products.Count();
+            if(count <= 0)
+            {
+                var prod = new Product
+                {
+                    Name = "Football Ball Nike",
+                    Color = "White Blue",
+                    Guarantee = "24 months",
+                    Price = 145.67,
+                    Available = true,
+                    Image = "https://1footballbible.com.ua/wp-content/uploads/2019/04/W0635_1115_main.jpg",
+                    CategoryId = _context.Categories.FirstOrDefault(x => x.CategoryName == "Football").Id
+                };
+                _context.Products.Add(prod);
+                _context.SaveChanges();
+            }
+        }
+
+        public static void SeedCategories(EFDbContext _context)
+        {
+            var count = _context.Categories.Count();
+            if(count <= 0)
+            {
+                var category = new Category
+                {
+                    CategoryName = "Football"
+                };
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+            }
+        }
+
+        public static void SeedUsers(UserManager<DbUser> _userManager,
+                                    EFDbContext _context)
+        {
+            var count = _userManager.Users.Count();
+
+            if (count <= 0)
+            {
+                string mail = "semen@ukr.net";
+                var roleName = "Admin";
+                var user = new DbUser
+                {
+                    Email = mail,
+                    UserName = mail,
+                    PhoneNumber = "+380991111111"
+                };
+
+                var result = _userManager.CreateAsync(user, "Qwerty-1").Result;
+
+                var userProfile = new UserProfile
+                {
+                    Id = user.Id,
+                    FirstName = "Semen",
+                    MiddleName = "Semenovuch",
+                    LastName = "Semenuk",
+                    RegistrationDate = DateTime.Now
+                };
+
+                _context.UserProfile.Add(userProfile);
+                _context.SaveChanges();
+                result = _userManager.AddToRoleAsync(user, roleName).Result;
+            }
+        }
+
+        public static void SeedData(IServiceProvider services, IHostingEnvironment env, IConfiguration config)
+        {
+            using (var scope = services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var user_manager = scope.ServiceProvider.GetRequiredService<UserManager<DbUser>>();
+                var managerRole = scope.ServiceProvider.GetRequiredService<RoleManager<DbRole>>();
+                var context = scope.ServiceProvider.GetRequiredService<EFDbContext>();
+
+                SeederDb.SeedCategories(context);
+                //SeederDb.SeedSportEquipment(context);
+                SeederDb.SeedRoles(managerRole);
+                //SeederDb.SeedUsers(user_manager, context);
+            }
+        }
+    }
+}
---
+++ ExamTask/DAL/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamTask.DAL.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public bool Available { get; set; }
+        public string Guarantee { get; set; }
+        public string Color { get; set; }
+        public string CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
